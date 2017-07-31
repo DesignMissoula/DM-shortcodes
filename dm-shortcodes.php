7dd8c2fdee15fd75d9eb8dc435d224d0181293bb
Bradford Knowlton <brad@ucanbonline.net>
2017-07-31 14:04:07 -0600

    added img, a, and br tags

--- dm-shortcodes.php
+++ dm-shortcodes.php
@@ -31,3 +31,45 @@ function span_shortcode( $atts, $content = null ) {
 	return '<span class="'.$a['class'].'">' . $content . '</span>';
 }
 add_shortcode( 'span', 'span_shortcode' );
+
+function a_shortcode( $atts, $content = null ) {
+	  $a = shortcode_atts( array(
+        'class' => 'a',
+        'href' => '#',
+		), $atts );
+		
+		$a['href'] = str_replace('"', "", $a['href']);
+		$a['href'] = str_replace("'", "", $a['href']);
+		
+		$a['class'] = str_replace('"', "", $a['class']);
+		$a['class'] = str_replace("'", "", $a['class']);
+		
+	return '<a class="'.$a['class'].'" href="'.$a['href'].'">' . $content . '</a>';
+}
+add_shortcode( 'a', 'a_shortcode' );
+
+
+function img_shortcode( $atts, $content = null ) {
+	  $a = shortcode_atts( array(
+        'class' => 'a',
+        'href' => '#',
+        'src' => '#',
+		), $atts );
+		
+		$a['href'] = str_replace('"', "", $a['href']);
+		$a['href'] = str_replace("'", "", $a['href']);
+		
+		$a['class'] = str_replace('"', "", $a['class']);
+		$a['class'] = str_replace("'", "", $a['class']);
+		
+		$a['src'] = str_replace('"', "", $a['src']);
+		$a['src'] = str_replace("'", "", $a['src']);
+		
+	return '<img class="'.$a['class'].'" src="'.$a['src'].'" />';
+}
+add_shortcode( 'img', 'img_shortcode' );
+
+function br_shortcode() {
+	return '<br />';
+}
+add_shortcode( 'br', 'br_shortcode' );
